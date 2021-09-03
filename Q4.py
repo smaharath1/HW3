163194f7ffe62425d08d52cdff3d5459f7c846da
Ubuntu <stevenm@steven-vm.u0mdbf3sdj1uxj5shit42bfglb.bx.internal.cloudapp.net>
2021-09-03 01:34:01 +0000

    Converts todict.csv to a dictionary

---
+++ Q4.py
@@ -0,0 +1,11 @@
+import csv
+def toDict():
+    my_dict = {"Title":[],"Author":[],"ISBN13":[],"Pages":[]}
+    reader = csv.DictReader(open('todict.csv'))
+    for row in reader:
+        my_dict['Title'].append(row["Title"])
+        my_dict['Author'].append(row["Author"])
+        my_dict['ISBN13'].append(row["ISBN13"])
+        my_dict['Pages'].append(row["Pages"])
+    print(my_dict)
+toDict()
